bde1f28396e00d4110120556cc71cc6593e4e7d7
John Clem <johnnyclem@gmail.com>
2013-07-15 16:25:33 -0700

    added addition admin_user_product_key controller methods to correspond to routes

--- app/controllers/spree/admin/users_controller_decorator.rb
+++ app/controllers/spree/admin/users_controller_decorator.rb
@@ -2,7 +2,38 @@ Spree::Admin::UsersController.class_eval do
 
   def generate_product_key
     user.product_key = SecureRandom.hex(24)
-    if user.save!
+    if user.save
+      flash[:success] = Spree.t('customer_details_updated')
+      redirect_to edit_admin_user_path(user)
+    else
+      render :action => :edit
+    end
+  end
+  
+  def activate_product_key
+    user.key_is_valid = true
+    if user.save
+      flash[:success] = Spree.t('customer_details_updated')
+      redirect_to edit_admin_user_path(user)
+    else
+      render :action => :edit
+    end
+  end
+  
+  def reset_activation_count
+    user.activation_count = 0
+    if user.save
+      flash[:success] = Spree.t('customer_details_updated')
+      redirect_to edit_admin_user_path(user)
+    else
+      render :action => :edit
+    end
+  end
+  
+  def invalidate_product_key
+    user.key_is_valid = false
+    user.product_key = ""
+    if user.save
       flash[:success] = Spree.t('customer_details_updated')
       redirect_to edit_admin_user_path(user)
     else
