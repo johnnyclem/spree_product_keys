d9346a90a58824ac38d2d5947bfce9e4aadd3070
John Clem <johnnyclem@gmail.com>
2013-07-15 15:13:41 -0700

    added additional methods to user_decorator

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -6,6 +6,31 @@ if Spree.user_class
       save!
     end
     
+    def invalidate_key
+      self.key_is_valid = false
+      save!
+    end
+    
+    def generate_key
+      self.product_key = "generated product key goes here"
+    end
+    
+    def reset_activation_count
+      self.activation_count = 0
+    end
+    
+    def increment_activation_count
+      if self.activation_count < 5
+        self.activation_count += 1
+      end
+    end
+
+    def decrement_activation_count
+      if self.activation_count > 0
+        self.activation_count -= 1
+      end
+    end
+    
     def valid_key
       return self.key_is_valid
     end
