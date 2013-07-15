d7b0b4a918e6cd7d8872e930ab383900b163ec8d
John Clem <johnnyclem@gmail.com>
2013-07-15 16:13:16 -0700

    testing user model

--- app/controllers/spree/admin/users_controller_decorator.rb
+++ app/controllers/spree/admin/users_controller_decorator.rb
@@ -3,6 +3,16 @@ Spree::Admin::UsersController.class_eval do
   def generate_product_key
     user.product_key = SecureRandom.hex(24)
     user.save!
+    update_user
+  end
+  
+  def update_user
+    authorize! :update, user
+    if user.update_attributes(params[:user])
+      respond_with(user, :status => 200, :default_template => :show)
+    else
+      invalid_resource!(user)
+    end
   end
 
   private
